0211c288ae397fb64d913d43871ba97a87ffe98d
Durga Venkata Sai Maneeswar Mutyala <durga.mutyala@vedantu.com>
2022-06-20 14:42:12 +0530

    ng-hide and ng-show

--- app.js
+++ app.js
@@ -2,4 +2,11 @@ var app = angular.module('maneesh', []);
 
 app.controller('justcontroller',['$scope', function($scope) {
     $scope.controllerName = 'Name from controller';
+    $scope.fname =' fname';
+    $scope.lname =' lname';
+    $scope.fullname = function() {
+        return $scope.fname+ " "+ $scope.lname;
+    }
+    $scope.coupon = 'MANEESH';
+    $scope.showCoupon = true;
 }]);
