b39540ac71892153cca9375068c58d9a250ae702
Durga Venkata Sai Maneeswar Mutyala <durga.mutyala@vedantu.com>
2022-06-20 14:56:30 +0530

    filters

--- app.js
+++ app.js
@@ -1,12 +1,45 @@
-var app = angular.module('maneesh', []);
+var app = angular.module("maneesh", []);
 
-app.controller('justcontroller',['$scope', function($scope) {
-    $scope.controllerName = 'Name from controller';
-    $scope.fname =' fname';
-    $scope.lname =' lname';
-    $scope.fullname = function() {
-        return $scope.fname+ " "+ $scope.lname;
-    }
-    $scope.coupon = 'MANEESH';
+app.controller("justcontroller", [
+  "$scope",
+  function ($scope) {
+    $scope.controllerName = "Name from controller";
+    $scope.fname = " fname";
+    $scope.lname = " lname";
+    $scope.fullname = function () {
+      return $scope.fname + " " + $scope.lname;
+    };
+    $scope.coupon = "MANEESH";
     $scope.showCoupon = true;
-}]);
+    $scope.users = [
+      {
+        name: "maneesh",
+        salary: 60000,
+      },
+      {
+        name: "mani",
+        salary: 20000,
+      },
+      {
+        name: "esh",
+        salary: 30000,
+      },
+      {
+        name: "mansh",
+        salary: 60000,
+      },
+    ];
+    $scope.numbers = [{
+        name : 'Ten',
+        nums : 10
+    },{
+        name : 'One',
+        nums : 1
+    },{
+        name : 'Five',
+        nums : 5
+    },
+]
+  },
+  
+]);
